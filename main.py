f6e9cbabeb3f4c9d6ee2f035a2eaef91c99d36fb
awkward-willy <willy1118t@gmail.com>
2025-10-19 17:47:44 +0800

    [Fix] Fix notation to fit document

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ x1 = Node(2)
 x2 = Node(5)
 
 # create computational graph and evaluate function value
-y = sub(add(log(x1), mul(x1, x2)), sin(x2))
+v1 = log(x1)
+v2 = mul(x1, x2)
+v3 = sin(x2)
+v4 = add(v1, v2)
+y = sub(v4, v3)
 
 # perform forward-mode autodiff
 forward(x1)
@@ -16,4 +20,4 @@ forward(x1)
 grads = reverse(y, trace=True)
 print("=== reverse gradients ===")
 for node, grad in grads.items():
-	print(f"d{y.name}/d{node.name} = {grad:.6f}")
+    print(f"d{y.name}/d{node.name} = {grad:.6f}")
